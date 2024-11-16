09aeed99435f49f4c59fc3f2a401cfcbafbfb68f
Ramiro Otermin <ramiro.o@geopagos.com>
2024-11-15 22:54:27 -0300

    Fix test filename

--- tests/Feature/AuthControllerTest.php
+++ tests/Feature/AuthControllerTest.php
@@ -5,7 +5,7 @@ namespace Tests\Feature;
 use Illuminate\Foundation\Testing\RefreshDatabase;
 use Tests\TestCase;
 
-class ExampleTest extends TestCase
+class AuthControllerTest extends TestCase
 {
     use RefreshDatabase;
 
